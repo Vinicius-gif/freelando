c895dee0d5c3544fd91ea3083850da9537cdf2fe
Vinicius <vfpv@hotmail.com>
2023-05-10 23:46:46 -0300

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,44 @@ import { Botao } from "./componentes/Botao/Botao";
 import { Cabecalho } from "./componentes/Cabecalho/Cabecalho";
 import { FreelandoLogo } from "./componentes/Icones/FreelandoLogo";
 import { Link } from "./componentes/Link/Link";
+import { ListaInline } from "./componentes/ListaInline/ListaInline";
+import { ItemListaInline } from "./componentes/ItemListaInline/ItemListaInline";
+import { IconeWhatsApp } from "./componentes/Icones/IconeWhatsApp";
+import { IconeTwitter } from "./componentes/Icones/IconeTwitter";
+import { IconeTwitch } from "./componentes/Icones/IconeTwitch";
+import { IconeInstagram } from "./componentes/Icones/IconeInstagram";
+import { Rodape } from "./componentes/Rodape/Rodape";
+import { ListaSuspensa } from "./componentes/ListaSuspensa/ListaSuspensa";
+
+const estadosBrasileiros = [
+  { "text": "Acre", "value": "AC" },
+  { "text": "Alagoas", "value": "AL" },
+  { "text": "Amapá", "value": "AP" },
+  { "text": "Amazonas", "value": "AM" },
+  { "text": "Bahia", "value": "BA" },
+  { "text": "Ceará", "value": "CE" },
+  { "text": "Distrito Federal", "value": "DF" },
+  { "text": "Espírito Santo", "value": "ES" },
+  { "text": "Goiás", "value": "GO" },
+  { "text": "Maranhão", "value": "MA" },
+  { "text": "Mato Grosso", "value": "MT" },
+  { "text": "Mato Grosso do Sul", "value": "MS" },
+  { "text": "Minas Gerais", "value": "MG" },
+  { "text": "Pará", "value": "PA" },
+  { "text": "Paraíba", "value": "PB" },
+  { "text": "Paraná", "value": "PR" },
+  { "text": "Pernambuco", "value": "PE" },
+  { "text": "Piauí", "value": "PI" },
+  { "text": "Rio de Janeiro", "value": "RJ" },
+  { "text": "Rio Grande do Norte", "value": "RN" },
+  { "text": "Rio Grande do Sul", "value": "RS" },
+  { "text": "Rondônia", "value": "RO" },
+  { "text": "Roraima", "value": "RR" },
+  { "text": "Santa Catarina", "value": "SC" },
+  { "text": "São Paulo", "value": "SP" },
+  { "text": "Sergipe", "value": "SE" },
+  { "text": "Tocantins", "value": "TO" }
+]
 
 function App() {
   return (
@@ -42,7 +80,7 @@ function App() {
               </Row>
               <Row>
                 <Col lg={4} md={4} sm={4}>
-                  <CampoTexto titulo='Cidade' />
+                  <ListaSuspensa titulo="Estado" opcoes={estadosBrasileiros}/> 
                 </Col>
                 <Col lg={8} md={8} sm={8}>
                   <CampoTexto titulo='Estado' />
@@ -79,6 +117,41 @@ function App() {
           </Col>
         </Row>
       </Container>
+      <Rodape>
+        <Container>
+          <Row align="center">
+            <Col>
+              <FreelandoLogo height={40} width={176} />
+              <Tipografia variante="legenda" componente="legenda">Desenvolvido por Alura. Projeto fictício sem fins comerciais.</Tipografia>
+            </Col>
+            <Col style={{ textAlign: 'right' }}>
+              <Tipografia variante="legenda" componente="legenda">Acesse nossas redes:</Tipografia>
+              <ListaInline>
+                <ItemListaInline>
+                  <a href="/" aria-label="Link para o WhatsApp">
+                    <IconeWhatsApp />
+                  </a>
+                </ItemListaInline>
+                <ItemListaInline>
+                  <a href="/" aria-label="Link para a Twitch">
+                    <IconeTwitch />
+                  </a>
+                </ItemListaInline>
+                <ItemListaInline>
+                  <a href="/" aria-label="Link para a Instagram">
+                    <IconeInstagram />
+                  </a>
+                </ItemListaInline>
+                <ItemListaInline>
+                  <a href="/" aria-label="Link para a Twitter">
+                    <IconeTwitter />
+                  </a>
+                </ItemListaInline>
+              </ListaInline>
+            </Col>
+          </Row>
+        </Container>
+      </Rodape>
     </ProvedorTema>
   );
 }
